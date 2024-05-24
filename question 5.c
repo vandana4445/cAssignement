f04fa2888fdf4066eeb88e51d2fec270205ac9e2
vandana4445 <vandanachaturvedi527@gmail.com>
2024-05-24 12:37:40 +0530

    Add files via upload

---
+++ question 5.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    // Write a program to print “Happy Teachers Day” on the screen (Remember to print double and single quotes).
+    //Vandana chaturvedi 
+    printf("\"Happy Teachers Day\"\n");
+    return 0;
+}
