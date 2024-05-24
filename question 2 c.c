05b21df2071ec4e117928d35ad90b2a2835cab54
vandana4445 <vandanachaturvedi527@gmail.com>
2024-05-24 11:24:34 +0530

    Add files via upload

---
+++ question 2 c.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // Print "Hello" in the first line and "KGI !!" in the second line.
+    //Vandana chaturvedi
+    printf("Hello\nKGI !!\n");
+    return 0;
+}
+}
